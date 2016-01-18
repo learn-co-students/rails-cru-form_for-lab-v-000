97178b998cf97376c7aa1400d5cabd1f9cbb22ca
Lukeghenco <lukeghenco@gmail.com>
2016-01-18 15:39:10 -0500

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,39 @@
+class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create(artist_params)
+    redirect_to artist_path(@artist)
+  end
+
+  def show
+    find_artist
+  end
+
+  def edit
+    find_artist
+  end
+
+  def update
+    find_artist
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
+  end
+
+  private
+
+    def artist_params
+      params.require(:artist).permit(:name, :bio)
+    end
+
+    def find_artist
+      @artist = Artist.find(params[:id])
+    end
+end
---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,38 @@
+class GenresController < ApplicationController
+  def index
+    @genres = Genre.all
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.create(genre_params)
+    redirect_to genre_path(@genre)
+  end
+
+  def show
+    find_genre
+  end
+
+  def edit
+    find_genre
+  end
+
+  def update
+    find_genre
+    @genre.update(genre_params)
+    redirect_to genre_path(@genre)
+  end
+
+  private
+
+    def genre_params
+      params.require(:genre).permit(:name)
+    end
+
+    def find_genre
+      @genre = Genre.find(params[:id])
+    end
+end
