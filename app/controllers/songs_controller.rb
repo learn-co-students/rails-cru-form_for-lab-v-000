581e98704c104dc084971846659541a143681029
James Novak <jamesnovak90@gmail.com>
2016-04-03 18:25:54 -0400

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,34 @@
+require 'pry'
+class SongsController < ApplicationController
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.create(post_params)
+    redirect_to song_path(@song)
+  end
+
+  def show
+    @song = Song.find(params[:id])
+    @artist = Artist.find(@song.artist_id)
+    @genre = Genre.find(@song.genre_id)
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(post_params)
+    redirect_to song_path(@song)
+  end
+
+  private
+
+  def post_params
+    params.require(:song).permit(:name, :artist_id, :genre_id)
+  end
+end
