f9f71e26f50248c12b4bc242491fcb1bb9797865
Joshua Azemoh <joshazemoh@gmail.com>
2016-04-28 23:42:35 +0100

    Build Artist CRU

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428212704) do
+ActiveRecord::Schema.define(version: 20160428221131) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "genres", force: :cascade do |t|
     t.string   "name"
