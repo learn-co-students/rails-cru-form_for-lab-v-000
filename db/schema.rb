996a1748e80ed262d4eae511b4b029f2e3e0242b
Joshua Azemoh <joshazemoh@gmail.com>
2016-04-28 23:56:16 +0100

    Build model associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428224408) do
+ActiveRecord::Schema.define(version: 20160428224938) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 20160428224408) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "genre_id"
+    t.integer  "artist_id"
   end
 
 end
