88197dc9b158c074571cd96300f1f44fe715dd1f
Joshua Azemoh <joshazemoh@gmail.com>
2016-04-28 23:47:29 +0100

    Generate Song resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :songs, only: [:new, :create, :show, :edit, :update]
   resources :artists, only: [:new, :create, :show, :edit, :update]
   resources :genres, only: [:new, :create, :show, :edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428221131) do
+ActiveRecord::Schema.define(version: 20160428224408) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20160428221131) do
   end
 
   create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
