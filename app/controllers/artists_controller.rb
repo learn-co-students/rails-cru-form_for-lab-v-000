3cf50a5e6a1499c1c765cc71d1dcb0dc95f7ab5f
seanliu93 <sean93liu@gmail.com>
2016-04-28 22:52:20 -0400

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,37 @@
+class ArtistsController < ApplicationController
+  def index
+  @artists = Artist.all
+  end
+
+  def new
+  @artist = Artist.new
+  end
+
+  def create
+  @artist = Artist.create(artist_params)
+  redirect_to artist_path(@artist)
+  end
+
+  def show
+  @artist = find
+  end
+
+  def edit
+  @artist = find
+  end
+
+  def update
+  @artist = find
+  @artist.update(artist_params)
+  redirect_to artist_path(@artist)
+  end
+
+  private
+  def find
+   Artist.find(params[:id])
+  end
+
+  def artist_params
+   params.require(:artist).permit(:name, :bio)
+  end
+end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,39 @@
+class SongsController < ApplicationController
+  def index
+   @songs = Song.all
+  end
+
+  def new
+   @song = Song.new
+   @artists = Artist.all
+   @genres = Genre.all
+  end
+
+  def create
+   @song = Song.create(song_params)
+   redirect_to song_path(@song)
+  end
+
+  def show
+   @song = find
+  end
+
+  def edit
+   @song = find
+  end
+
+  def update
+   @song = find
+   @song.update(song_params)
+   redirect_to song_path(@song)
+  end
+
+  private
+   def find
+     Song.find(params[:id])
+   end
+
+   def song_params
+     params.require(:song).permit(:name, :artist_id, :genre_id)
+   end
+end
