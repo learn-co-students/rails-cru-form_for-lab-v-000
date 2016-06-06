f785895e94f24bba19f1742fa9261c17b83adee4
cassaram09 <cassaram09@gmail.com>
2016-06-06 18:01:41 -0400

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,38 @@
+class GenresController < ApplicationController
+
+  def index
+    @genres = Genre.all
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params(:name, :bio))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+  def show
+    @genre = Genre.find_by(id: params[:id])
+    @songs = @genre.songs
+  end
+
+  def edit
+    @genre = Genre.find_by(id: params[:id])
+  end
+
+  def update
+    @genre = Genre.find_by(id: params[:id])
+    @genre.update(genre_params(:name, :bio))
+    redirect_to genre_path(@genre)
+  end
+
+  private
+
+  def genre_params(*args)
+    params.require(:genre).permit(*args)
+  end
+
+end
