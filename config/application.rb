c6193e678ccf4fb26e763fc9ed6f6138552546a2
reichardn <reichardn@gmail.com>
2016-07-02 01:05:51 -0400

    Done.

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module BeatsByRails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators.test_framework false
+
   end
 end
