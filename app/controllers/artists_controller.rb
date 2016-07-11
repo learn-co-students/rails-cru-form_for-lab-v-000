ee5e1da012da3a71beaaa3629ed370a4916aa03e
mcasciato <michael.casciato@icloud.com>
2016-07-11 18:06:58 -0400

    new artist form done

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,18 @@
 class ArtistsController < ApplicationController
 
+  def new
+    @artist = Artist.new
+  end
 
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
 
+  private
+
+  def artist_params(*args)
+    params.require(:artist).permit(:name, :bio)
+  end
 end
