9fc4f97f08ce5100b8bd8e3ee01eb169e6c4f655
mcasciato <michael.casciato@icloud.com>
2016-07-11 21:34:03 -0400

    all artist tests passing

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -10,6 +10,20 @@ class ArtistsController < ApplicationController
     redirect_to artist_path(@artist)
   end
 
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
+  end
+
   private
 
   def artist_params(*args)
