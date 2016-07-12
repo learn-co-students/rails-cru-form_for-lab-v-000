c71a69d12fa0b805bd99737f6047b0b6fa80803a
mcasciato <michael.casciato@icloud.com>
2016-07-11 21:52:47 -0400

    genre controller done

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,6 +1,33 @@
 class GenresController < ApplicationController
 
+  def new
+    @genre = Genre.new
+  end
 
+  def show
+    @genre = Genre.find(params[:id])
+  end
 
+  def create
+    @genre = Genre.new
+    @genre.save
+    redirect_to genres_path(@genre)
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to genres_path(@genre)
+  end
+
+  private
+
+  def genre_params(*args)
+    params.require(:genre).permit(:name)
+  end
 
 end
