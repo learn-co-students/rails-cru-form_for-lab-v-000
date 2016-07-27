f4984152dd71cf0e1e2ffb21241f459e720ba853
jchu4483 <jchu4483@gmail.com>
2016-07-27 02:37:51 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,6 @@
 class SongsController < ApplicationController
   def index
-
+    @songs = Song.all
   end
 
   def show
@@ -23,6 +23,7 @@ class SongsController < ApplicationController
   def update
     @song = Song.find(params[:id])
     @song.update(song_params(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
   end
 
 
