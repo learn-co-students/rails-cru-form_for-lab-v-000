33487be5aca7e6b41aea5de482463fcb1b26e3ca
Jason <jasonkwong11@gmail.com>
2016-08-01 00:22:46 -0400

    done

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,39 @@
+class ArtistsController < ApplicationController
+
+  def new
+    @artist = Artist.new
+  end
+
+
+  def show
+   @artist = Artist.find(params[:id])
+  end
+
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params(:name, :bio))
+    @artist.save
+
+    redirect_to artist_path(@artist)
+  end
+
+  def create
+    @artist = Artist.new(artist_params(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+   def new
+    @artist = Artist.new
+  end
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
+
+end
