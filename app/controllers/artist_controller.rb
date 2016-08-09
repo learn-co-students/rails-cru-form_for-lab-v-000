14e92943ec798455ed0b76a238d1d081c6fb768e
rafeeq-123 <rafeeq-123@hotmail.com>
2016-08-09 16:54:34 -0700

    Done.

--- app/controllers/artist_controller.rb
+++ app/controllers/artist_controller.rb
@@ -1,6 +1,13 @@
 class ArtistsController < ApplicationController
 
+  def index
+    @artist = Artist.all
+  end
 
+  def new
+    @artist = Artist.new
+    #redirect_to artist_post(@artist)
+  end
 
 #   def show
 #     @artist = Artist.find(params[:id])
