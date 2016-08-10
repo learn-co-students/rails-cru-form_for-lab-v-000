38f92ecdbf750953bdf182522af60c5c45f477e7
jd2rogers2 <jd2rogers2@gmail.com>
2016-08-10 10:48:49 -0400

    donzo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
+
+  def genre_params(*args)
+    params.require(:genre).permit(*args)
+  end
 end
