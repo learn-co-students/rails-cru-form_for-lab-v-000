c3e8510ec019c03eb9e89c16b049974b4d7449f3
Lenn Hypolite <lenntriniman@hotmail.com>
2016-09-23 21:59:37 +0000

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :songs [:show, :get, :edit, :destroy, :update]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
