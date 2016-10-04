e0056ea477ca950e0e227cf0288426587ba9ef01
bmolina-nyc <bruce.d.molina@gmail.com>
2016-10-03 21:13:54 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,5 +8,34 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
   end
 
+  def new 
+    @song = Song.new
+  end
+
+  def edit 
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.create(post_params(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
+  end
+
+  def update 
+    @song = Song.find(params[:id])
+    @song.update(post_params(:name, :artist_id, :genre_id))
+    @song.save 
+
+    redirect_to song_path(@song)
+  end
+
+
+  private 
   
+  def post_params(*args)
+    params.require(:song).permit(*args)
+  end
+
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     resources :artists, only: [:show, :new, :create, :edit, :update]
-    resources :songs, only: [:index, :show]
+    resources :genres, only: [:show, :new, :create, :edit, :update]
+    resources :songs, only: [:index, :show, :new, :create, :edit, :update]
 
 # :show -> 
 # Prefix Verb URI Pattern            Controller#Action
