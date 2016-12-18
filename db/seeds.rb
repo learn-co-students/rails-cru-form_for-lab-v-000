7d1dc35bee80f9c05357af7a468090fe9d22fac5
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-12-18 21:47:16 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Song.create(name: "lecho dodi")
+Genre.create(name: "pop")
+Artist.create(name: "abe")
