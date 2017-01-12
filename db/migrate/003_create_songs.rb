7a435d512f8bf8d22206ba50f8f12af586daf930
NATHAN ROBBERT <admin@bigrocketgames.com>
2017-01-12 05:15:45 +0000

    Done.

---
+++ db/migrate/003_create_songs.rb
@@ -0,0 +1,13 @@
+class CreateSongs < ActiveRecord::Migration
+
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.integer :artist_id
+      t.integer :genre_id
+
+      t.timestamps null: false
+    end
+  end
+
+end
