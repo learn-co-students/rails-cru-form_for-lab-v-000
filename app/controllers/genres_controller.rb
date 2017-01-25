203ac11d27db7d9871c1fb3698a8c183d12b66ee
Senad Hadrovic <github email address>
2017-01-25 01:10:56 -0800

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,28 @@
+class GenresController < ApplicationController
+
+	def new
+	 @genre = Genre.new
+	end
+
+	def create
+	 @genre = Genre.new	
+     @genre.name = params[:genre][:name]
+     @genre.save
+     redirect_to genre_path(@genre)
+	end
+
+	def edit
+		@genre = Genre.find(params[:id])
+	end
+
+	def update
+		@genre = Genre.find(params[:id])
+	    @genre.update(name: params[:genre][:name])
+	    redirect_to genre_path(@genre)
+	end
+
+	def show
+		@genre = Genre.find(params[:id])
+	end
+
+end
