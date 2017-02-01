393d4d816a659c18d59928d3b6783c3522dfcce3
Adam Lucia <lucia.adam@gmail.com>
2017-02-01 06:46:35 +0000

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,16 @@
+class GenresController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,16 @@
+class SongsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
