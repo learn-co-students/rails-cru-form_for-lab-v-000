6d1597747ea88c49181878f856444bc4a14b6788
vanita eckert <vanita112003@gmail.com>
2017-03-12 23:26:55 +0000

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,36 @@
+class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find_by(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+      # binding.pry
+    @artist = Artist.new(params.require(:artist).permit(:name, :bio))
+    @artist.save
+
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find_by(params[:id])
+  end
+
+  def update
+    @artist = Artist.find_by(params[:id])
+    @artist.update(params.require(:artist).permit(:name, :bio))
+    
+
+   redirect_to artist_path(@artist)
+  end
+
+
+end
