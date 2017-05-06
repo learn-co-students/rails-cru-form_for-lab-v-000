c790d3f4be4cfe8bff62dd92355368c03ba1d6c7
PhreeMason <mr.omarmason@gmail.com>
2017-05-06 05:57:39 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,11 +4,25 @@ class ArtistsController < ApplicationController
   end
   
   def new
-    @artist = Artist.new
+   @artist = Artist.new
   end
   def create
-    @artist = Artist.create(params.require(:artist).permit(:name, :bio)
-    @artist.save
-     redirect_to 
+   @artist = Artist.create(params.require(:artist).permit(:name, :bio))
+   @artist.save
+   redirect_to artist_path(@artist) 
   end
+  
+  def edit
+   @artist = Artist.find(params[:id])
+  end
+  
+  def update 
+   @artist = Artist.find(params[:id])
+   @artist.update(params.require(:artist).permit(:name, :bio))
+   @artist.save
+   redirect_to artist_path(@artist) 
+  end
+  
+  
+  
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,26 @@
 class SongsController < ApplicationController
+  def show
+   @song = Song.find(params[:id])
+  end
+  
+  def new
+   @song = Song.new
+  end
+  def create
+   @song = Song.create(params.require(:song).permit(:name, :bio))
+   @song.save
+   redirect_to song_path(@song) 
+  end
+  
+  def edit
+   @song = Song.find(params[:id])
+  end
+  
+  def update 
+   @song = Song.find(params[:id])
+   @song.update(params.require(:song).permit(:name, :bio))
+   @song.save
+   redirect_to song_path(@song) 
+  end
+  
 end
