638c538eb76088d5722f37ec285939a6347184f0
Feiran Wang <wangfeiranpku@163.com>
2017-05-06 09:09:36 +0800

    Done.

--- spec/controllers/genres_controller_spec.rb
+++ spec/controllers/genres_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-# RSpec.describe GenresController, type: :controller do
+RSpec.describe GenresController, type: :controller do
 
 end
--- spec/controllers/songs_controller_spec.rb
+++ spec/controllers/songs_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-# RSpec.describe SongsController, type: :controller do
+RSpec.describe SongsController, type: :controller do
 
 end
