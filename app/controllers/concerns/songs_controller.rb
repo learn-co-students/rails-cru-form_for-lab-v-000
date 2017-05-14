d50424a5ab5941bcdaaf2112b2a8df577e1ad724
github username <github email address>
2017-05-14 16:13:49 -0400

    Done.

---
+++ app/controllers/concerns/songs_controller.rb
@@ -0,0 +1,11 @@
+class SongsController < ApplicationController
+
+	def create
+	end
+
+	def update
+	end
+
+	def show
+	end
+end
---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,27 @@
+class GenresController < ApplicationController
+
+	def create
+		@genre = Genre.new(params.require(:genre).permit(:name))
+		@genre.save
+		redirect_to genre_path(@genre)
+	end
+
+	def update
+		@genre = Genre.find(params[:id])
+		@genre.update(params.require(:genre).permit(:name))
+		redirect_to genre_path(@genre)
+	end
+
+	def show
+		@genre = Genre.find(params[:id])
+	end
+
+	def new
+		@genre = Genre.new
+	end
+
+	def edit
+		@genre = Genre.find(params[:id])
+	end
+
+end
