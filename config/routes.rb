1853560a1f65803de08def598982d3816a8a8e75
root <dschlaud@gmail.com>
2017-07-08 23:42:15 -0500

    create models, controllers, view folders

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :songs
   resources :genres
   resources :artists
+  resources :songs
+  resources :genres
+  resources :artists
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708172032) do
+ActiveRecord::Schema.define(version: 20170709043844) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
