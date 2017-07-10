52a455dd61f0ec9f2ecb07c029f3196f8ead7ca7
Yevgeniy Skroznikov <ohnap@hotmail.com>
2017-07-10 14:34:57 +0000

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,29 @@
+class GenresController < ApplicationController
+  def index
+    @genres = Genre.all
+  end
+
+  def show
+    @genre = Genre.find_by(id: params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.create(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    @genre = Genre.find_by(id: params[:id])
+  end
+
+  def update
+    @genre = Genre.find_by(id: params[:id])
+    @genre.update(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre)
+  end
+
+end
