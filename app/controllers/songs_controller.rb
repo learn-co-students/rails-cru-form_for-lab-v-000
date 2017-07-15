76d658799e46d26a4fdd0afe0c73a386f97ecd5a
Will-Sauer <will@heueso.com>
2017-07-15 11:02:23 -0700

    Finished

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,39 @@
 class SongsController < ApplicationController
+
+def index
+@songs = Song.all
 end
+
+def new
+@song = Song.new
+end
+
+def create
+    @song = Song.new(form_params(:name, :genre_id, :artist_id))
+    @song.save
+redirect_to song_path(@song)
+end
+
+def show
+@song = Song.find(params[:id])
+end
+
+def edit
+@song = Song.find(params[:id])
+end
+
+def update
+    @song = Song.find(params[:id])
+    @song.update(form_params(:name, :genre_id, :artist_id))
+    @song.save
+redirect_to song_path(@song)
+end
+
+
+private
+def form_params(*args)
+params.require(:song).permit(args)
+end
+
+
+end#eoc
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :songs, only: [:new, :create, :show, :edit, :update]
+  resources :songs, only: [:new, :create, :show, :edit, :update, :index]
   resources :genres, only: [:new, :create, :show, :edit, :update]
   resources :artists, only: [:new, :create, :show, :edit, :update]
   end
