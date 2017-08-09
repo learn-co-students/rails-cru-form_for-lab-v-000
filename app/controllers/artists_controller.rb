9b8eddaa807ca05fc9876a7b60cf455ea29da1c3
Lisa Huang <lisa.yc.huang@gmail.com>
2017-08-09 15:50:38 -0700

    Updated ArtistsController with #new #create, and corresponding artists/new view

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,4 +3,14 @@ class ArtistsController < ApplicationController
     def show
         @artist = Artist.find_by(params[:id])
     end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        @artist = Artist.create(params.require(:artist).permit(:name, :bio))
+        @artist.save
+        redirect_to artist_path(@artist)
+    end
 end
