8cb491e92acb41ecf2d7a115d7151028532cb1a7
Margarita Khodzka <mkhodzka@gmail.com>
2017-08-15 00:54:18 +0000

    completed the Genre part

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   resources :artists, only: [:show, :new, :create, :edit]
   patch 'artists/:id', to: 'artists#update'
+
+  resources :genres, only: [:show, :new, :create, :edit]
+  patch 'genres/:id', to: 'genres#update'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
