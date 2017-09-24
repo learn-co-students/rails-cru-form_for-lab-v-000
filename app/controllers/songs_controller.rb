12494bd71a61cda2fb69773a045fdf1cbdc12f75
charlie lee <ctlee9096@gmail.com>
2017-09-24 18:18:16 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,23 @@
+class SongController < ApplicationController
+  def create
+    @song = Song.new(song_params)
+    @song.save
+    redirect_to song_path(@song)
+  end
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params)
+    redirect_to song_path(@song)
+  end
+  def show
+      @song = Song.new
+  end
+  def index
+    @song = Song.all 
+  end
+private
+
+def song_params
+  params.require(:song).permit(:name, :artist_id, :genre_id)
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  resources :genres
+  #resources :artists
+  resources :artists
+  resources :songs
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'song#show'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
