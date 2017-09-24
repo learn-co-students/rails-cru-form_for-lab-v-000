2ff4679db5b7d9015d82b36dc2751fc7009cfdd8
charlie lee <ctlee9096@gmail.com>
2017-09-24 18:51:55 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,26 @@
 class ArtistsController < ApplicationController
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
+  end
+  def show
+      @artist = Artist.new
+  end
+  def index
+    @artist = Artist.all
+  end
+  def new
+    @artist = Artist.new
+  end
+private
+
+def artist_params
+  params.require(:artist).permit(:name, :bio)
+end
 end
