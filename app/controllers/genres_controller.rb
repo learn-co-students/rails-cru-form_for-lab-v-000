ac8559e63641922c2178526889c82db4bb95ebd1
dwyn <dwayne.harmon@gmail.com>
2017-10-07 21:12:55 -0400

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,34 @@
+class GenresController < ApplicationController
+	def index
+		@genres = Genre.all
+	end
+
+	def show
+		@genre = Genre.find(params[:id])
+	end
+
+	def new
+		@genre = Genre.new
+	end
+
+	def create
+	  @genre = Genre.new(params.require(:genre).permit(:name))
+	  @genre.save
+	  redirect_to genre_path(@genre)
+	end
+
+	def update
+	  @genre = Genre.find(params[:id])
+	  @genre.update(params.require(:genre).permit(:name))
+	  redirect_to genre_path(@genre)
+	end
+
+	def edit
+	  @genre = Genre.find(params[:id])
+	end
+
+	private
+		def post_params(*args)
+		  params.require(:post).permit(*args)
+		end
+end
