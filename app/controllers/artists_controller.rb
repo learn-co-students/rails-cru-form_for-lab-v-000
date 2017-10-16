c84b3cbd5f444ca092bb164154ff3af3b96b1518
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-10-15 23:58:19 -0700

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,24 @@
+class ArtistsController < ApplicationController
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    artist = Artist.find(params[:id])
+    artist.update(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(artist)
+  end
+end
