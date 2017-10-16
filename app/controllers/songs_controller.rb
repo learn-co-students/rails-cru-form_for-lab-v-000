0ecbd7390e397428a9f3637ed53a91870089bda6
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-10-16 00:01:09 -0700

    Done

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,6 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
   
   def new
@@ -24,6 +25,6 @@ class SongsController < ApplicationController
   def update
     song = Song.find(params[:id])
     song.update(params.require(:song).permit(:name, :artist_id, :genre_id))
-    redirect_to song_path(@song)
+    redirect_to song_path(song)
   end
 end
