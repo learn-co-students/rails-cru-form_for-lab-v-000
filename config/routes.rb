ff5c4d7b161f34c7ab0d273a13fa69a9d6d04776
Dalia <daliasawaya@tests-MacBook-Air-2.local>
2017-10-19 20:32:17 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :songs
+  resources :genres
+  resources :artists
 end
