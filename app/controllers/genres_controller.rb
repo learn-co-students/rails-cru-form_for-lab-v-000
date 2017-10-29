4181e9e5e356d179638f51874574bd234a215127
lpassamano <leighayna.josele@gmail.com>
2017-10-29 15:35:40 -0400

    stubbed CRUD controller actions for Song, Artist, Genre

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,21 @@
 class GenresController < ApplicationController
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,21 @@
 class SongsController < ApplicationController
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
 end
