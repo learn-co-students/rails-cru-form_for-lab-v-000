b576e6a3f0702948fbd53daa815cf3d8b1bb161f
lpassamano <leighayna.josele@gmail.com>
2017-10-29 15:56:29 -0400

    added Genre new form view and controller actions

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,11 +4,13 @@ class GenresController < ApplicationController
   end
 
   def new
-
+    @genre = Genre.new
   end
 
   def create
-
+    @genre = Genre.new(post_params(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
   end
 
   def edit
@@ -17,5 +19,11 @@ class GenresController < ApplicationController
 
   def update
 
+  end
+
+  private
+
+  def post_params(*args)
+    params.require(:genre).permit(*args)
   end
 end
