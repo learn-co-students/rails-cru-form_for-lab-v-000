2e801661f10d3f47ce6db46bc0a46daa43f20468
lpassamano <leighayna.josele@gmail.com>
2017-10-29 16:13:58 -0400

    added Song edit form view and controllers

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,11 +14,13 @@ class SongsController < ApplicationController
   end
 
   def edit
-
+    @song = Song.find(params[:id])
   end
 
   def update
-
+    @song = Song.find(params[:id])
+    @song.update(post_params(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
   end
 
   private
