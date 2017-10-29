2625ac56104c93ef7cc46267f62bcfdca6f2b82c
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-10-28 22:09:43 -0700

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,21 +1,36 @@
 class ArtistsController < ApplicationController
 
-  def index
-  end
+    def index
+    end
 
-  def show
-  end
+    def show
+      @artist = Artist.find(params[:id])
+    end
 
-  def new
-  end
+    def new
+      @artist = Artist.new
+    end
 
-  def create
-  end
+    def create
+      @artist = Artist.new(artist_params)
+      @artist.save
+      redirect_to artist_path(@artist)
+    end
 
-  def edit
-  end
+    def edit
+      @artist = Artist.find(params[:id])
+    end
 
-  def update
-  end
+    def update
+      @artist = Artist.find(params[:id])
+      @artist.update(artist_params)
+      redirect_to artist_path(@artist)
+    end
 
-end
+    private
+
+    def artist_params
+      params.require(:artist).permit(:name)
+    end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :artist, only: [:index, :show, :new, :create, :edit, :update]
-  get '/artists/:id' => 'artists#show'
-  resources :genre, only: [:index, :show, :new, :create, :edit, :update]
-  get '/genres/:id' => 'genres#show'
+  resources :artists, only: [:index, :show, :new, :create, :edit, :update]
+  get '/artists/:id' => 'artist#show'
+  resources :genres, only: [:index, :show, :new, :create, :edit, :update]
+  get '/genres/:id' => 'genre#show'
   resources :songs, only: [:index, :show, :new, :create, :edit, :update]
   get '/songs/:id' => 'songs#show'
 end
