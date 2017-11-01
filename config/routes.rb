a889a49680f1a9a047241f04c3b367140c8d3ab9
Cole Martin <mar08049@gmail.com>
2017-11-01 15:38:48 +0000

    Revert "Done."
    This reverts commit c2034754c5a9966e5ece65bfd4b1497963884906.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  resources :songs
-  resources :genres
-  resources :artists
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
