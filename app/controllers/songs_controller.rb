7252307aa7679979b07f5760088ecd5b1fad451a
Jacqlene Sifre <jsifre01@gmail.com>
2017-11-14 00:57:16 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,34 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(songs_params(:name, :artist_id, :genre_id))
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(songs_params(:name, :genre_id, :artist_id))
+    redirect_to song_path(@song)
+  end
+
+  private
+    def songs_params(*args)
+      params.require(:song).permit(*args)
+    end
+end
