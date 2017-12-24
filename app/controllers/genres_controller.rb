14cae37174f9073d0645c3c98b8d4ae3c17bbebf
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2017-12-24 21:34:21 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -13,4 +13,15 @@ class GenresController < ApplicationController
         @genre.save 
         redirect_to genre_path(@genre)
     end 
+    
+    def edit
+        @genre = Genre.find(params[:id])
+    end 
+    
+    def update 
+        @genre = Genre.find(params[:id])
+        @genre.update(params.require(:genre).permit(:name))
+        redirect_to genre_path(@genre)
+    end 
+    
 end
