8a2bbda105a5e5452e549b4ab6113926f1142339
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-27 05:32:10 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,14 +1,20 @@
 class ArtistsController < ApplicationController
   def new
+    @artist = Artist.new
   end
 
   def create
+    @artist = Artist.create(artist_params(:name, :bio))
   end
 
   def edit
+    @artist = Artist.find(params[:id])
   end
 
   def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params(:name, :bio))
+    redirect_to artist_path(@artist)
   end
 
   def destroy
@@ -19,5 +25,11 @@ class ArtistsController < ApplicationController
 
   def show
     @artist = Artist.find(params[:id])
+  end
+
+  private
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
   end
 end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,14 +1,20 @@
 class GenresController < ApplicationController
   def new
+    @genre = Genre.new
   end
 
   def create
+    @genre = Genre.create(genre_params(:name))
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params(:name))
+    redirect_to genre_path(@genre)
   end
 
   def destroy
@@ -18,5 +24,11 @@ class GenresController < ApplicationController
   end
 
   def show
+    @genre = Genre.find(params[:id])
+  end
+
+  private
+  def genre_params(*args)
+    params.require(:genre).permit(*args)
   end
 end
