0e2bedf907b19fce795bc25ad4c8381535360019
Yvonne Dreptate <YvonneDreptate@gmail.com>
2018-01-02 16:36:33 -0500

    Done.

---
+++ db/migrate/20180102194627_create_artists.rb
@@ -0,0 +1,11 @@
+class CreateArtists < ActiveRecord::Migration
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.text :bio
+
+      t.timestamps null: false
+    end
+  end
+end
