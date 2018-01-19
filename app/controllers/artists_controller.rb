b3dd961fbc9e7e525929f8e095ba0c8d58a29f48
Donnadieu <donnadieu.86@gmail.com>
2018-01-19 12:42:08 -0500

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,32 @@
+class ArtistsController < ApplicationController
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(strong_params)
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(strong_params)
+    redirect_to artist_path(@artist)
+  end
+
+  private
+
+  def strong_params
+    params.require(:artist).permit(:name, :bio)
+  end
+end
