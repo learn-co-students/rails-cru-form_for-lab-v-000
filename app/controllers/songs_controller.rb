81bab5abce0db4a88344dc036903f317f61dc185
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-01-23 02:14:31 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,6 +14,15 @@ class SongsController < ApplicationController
     redirect_to song_path(@song)
   end
 
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
+  end
 private
   def song_params(*args)
     params.require(:song).permit(*args)
