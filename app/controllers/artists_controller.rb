2f59d823e317f3baea7ab7533e0104d0ab489754
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-01-23 01:18:37 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,4 +3,21 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find(params[:id])
   end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+
+  private
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
 end
