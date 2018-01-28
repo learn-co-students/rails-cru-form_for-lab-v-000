d1e6bef72f02a8d0120331ae87d7dd706c98e4c0
Hannah Carnes <hecarnes@gmail.com>
2018-01-27 19:19:51 -0500

    done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,32 @@
 class GenresController < ApplicationController
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def create
+    @genre = Genre.new(params.require(:genre).permit(:name))
+    if @genre.save
+      redirect_to genre_path(@genre)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(params.require(:genre).permit(:name))
+      redirect_to genre_path(@genre)
+    else
+      render :edit
+    end
+  end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,41 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def song_params
+    params.require(:song).permit(:name, :artist_id, :genre_id)
+  end
 end
