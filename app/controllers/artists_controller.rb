49cab4fca0c050d0f72e7a07ed90810d66285c09
Benny Hudson Hung <wakenightryu@gmail.com>
2018-02-20 04:14:59 +0000

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,37 @@
+class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  private
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
+
+end
