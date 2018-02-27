d9c6648a931cf9951db2746a1d04ced599506d92
Nolan Byrnes <nolanehs@gmail.com>
2018-02-27 10:51:43 +0000

    Done.

---
+++ db/migrate/20180227094658_add_name_to_songs.rb
@@ -0,0 +1,7 @@
+class AddNameToSongs < ActiveRecord::Migration
+  def change
+    add_column :songs, :name, :string
+    add_column :songs, :artist_id, :integer
+    add_column :songs, :genre_id, :integer
+  end
+end
