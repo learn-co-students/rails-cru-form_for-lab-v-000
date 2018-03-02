b73d0100769e2631293eb22b7e2a74745e9cc7d1
mbalsamo <github email address>
2018-03-02 17:10:57 -0500

    complete artists model and views

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,22 @@
+class SongsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
