236b36a826f0848a39883a569a99e70184cb7d2c
churchwerks <steven.lennis@icloud.com>
2018-03-06 16:55:24 -0600

    Refactored Join Table.

--- db/schema.rb
+++ db/schema.rb
@@ -28,8 +28,6 @@ ActiveRecord::Schema.define(version: 20180306220134) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "name"
-    t.integer  "artist_id"
-    t.integer  "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
