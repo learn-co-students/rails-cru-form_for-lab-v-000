7288a613ea0bf7e5bae51045221914eb96ba3695
funemployeddad <funemployeddad@gmail.com>
2018-03-07 16:05:12 -0700

    built /artists to show all artists, with list of songs for each. each song points to it's song page. going to do the same for the artist name

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,28 @@
 class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+    set_artist
+  end
+
+  def update
+    set_artist
+  end
+
+  private
+
+  def set_artist
+    @artist = Artist.find(:id)
+  end
+
+
 end
