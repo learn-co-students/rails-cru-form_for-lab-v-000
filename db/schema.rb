27ff759ea4d0256bb45c7b30c104dedb77cd1323
jacobsilver2 <jacobsilver2@mac.com>
2018-03-11 19:03:21 -0400

    generated Genre resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180311230053) do
+ActiveRecord::Schema.define(version: 20180311230249) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
