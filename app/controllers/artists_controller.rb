dfc070210ca44535ed01cdbed656a9c8f0e0c5fa
jacobsilver2 <jacobsilver2@mac.com>
2018-03-11 19:23:44 -0400

    did a bunch of work on artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,22 @@
 class ArtistsController < ApplicationController
+  def show
+    @artist = Artist.find_by_id(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(params.require(:artist))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  private
+
+def post_params
+  params.require(:artist).permit(:name, :bio)
+end
+
 end
