b49e0089f8cfef09be8a5354f281052fa1642632
jacobsilver2 <jacobsilver2@mac.com>
2018-03-11 21:50:37 -0400

    finished songs controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -13,9 +13,13 @@ class ArtistsController < ApplicationController
     redirect_to artist_path(@artist)
   end
 
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
   def update
     @artist = Artist.find(params[:id])
-    @artist.update(params.require(:artist))    
+    @artist.update(artist_params)    
     redirect_to artist_path(@artist) 
   end
 
@@ -26,4 +30,3 @@ private
   end
 
 end
-
