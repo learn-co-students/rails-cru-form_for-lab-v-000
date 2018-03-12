e2a65e82461ea1bb79fa1bbb9f992be30610a3be
Alexander Urizar <urizaraj@gmail.com>
2018-03-12 21:09:51 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,26 @@
 class ArtistsController < ApplicationController
+  before_action :set_resource, only: [:show, :edit]
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    artist = Artist.create(strong_params)
+    redirect_to artist_path(artist)
+  end
+
+  def update
+    artist = Artist.update_all(strong_params)
+    redirect_to artist_path(artist)
+  end
+
+  private
+  def strong_params
+    params.require(:artist).permit(:name, :bio)
+  end
+
+  def set_resource
+    @artist = Artist.find(params[:id])
+  end
 end
