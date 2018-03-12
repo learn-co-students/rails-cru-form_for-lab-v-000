83c66e26b321bcaf3820084d06d711cc45c70fdf
Alexander Urizar <urizaraj@gmail.com>
2018-03-12 21:24:26 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,30 @@
 class SongsController < ApplicationController
+  before_action :set_resource, only: [:show, :edit]
+
+  def index
+    @songs = Song.all
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    song = Song.create(strong_params)
+    redirect_to song_path(song)
+  end
+
+  def update
+    song = Song.update_all(strong_params)
+    redirect_to song_path(song)
+  end
+
+  private
+  def strong_params
+    params.require(:song).permit(:name, :artist_id, :genre_id)
+  end
+
+  def set_resource
+    @song = Song.find(params[:id])
+  end
 end
