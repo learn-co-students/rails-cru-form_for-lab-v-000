a033aa805700811c7721f2e479faf0ca48427225
Allyson Wesman <allysonwesman@gmail.com>
2018-03-31 08:57:37 -0700

    Done

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,11 +13,6 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-    #new_song_parameters = new_song_parameters 
-    # { "song" => { "name" => "Franklin's Song", "genre_id" => 1, "artist_id" => 1}}
-    # @song.name = params[:song][:name]
-    # @song.genre = Genre.find(params[:song][:genre_id])
-    # @song.artist = Artist.find(params[:song][:artist_id])
     @song.save
 
     redirect_to song_path(@song.id)
@@ -29,11 +24,8 @@ class SongsController < ApplicationController
 
   def update
     @song = Song.find(params[:id])
-    name = params[:song][:name]
-    genre = Genre.find(params[:song][:genre_id])
-    artist = Artist.find(params[:song][:artist_id])
-
-    @song.update(name: name, genre: genre, artist: artist)
+    @song.update(song_params)
+    
     redirect_to song_path(@song.id)
   end
 
