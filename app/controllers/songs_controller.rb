8906fd7b6788ecb46ebc9d8071f72c7e11ed097a
Katrina Brinson <kbrinso@gmail.com>
2018-04-01 00:11:17 +0000

    manual scaffolding setup complete

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,27 @@
+class SongsController < ApplicationController
+  def show
+   @song = Song.find(params[:id])
+   @genre = @song.genre
+   @artist = @song.artist
+  end
+
+  def new
+     @song = Song.new
+  end
+
+  def create
+   @song = Song.new(params.require(:song).permit(:name, :artist_id, :genre_id))
+   @song.save
+   redirect_to artist_path(@song)
+  end
+
+  def edit
+   @song = Song.find(params[:id])
+  end
+
+  def update
+   @song = Song.find(params[:id])
+   @song.update(params.require(:song).permit(:name, :artist_id, :genre_id))
+    redirect_to artist_path(@song)
+  end
+end
