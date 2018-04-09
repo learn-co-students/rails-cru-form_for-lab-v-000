a039195f9a7f6b94b94ed8ab03e2438672fdf82a
Chely Ho <adept_inept@yahoo.com>
2018-04-09 16:13:05 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,21 +1,27 @@
 class ArtistsController < ApplicationController
-  # def index
-  # end
 
   def show
     set_artist
   end
 
   def new
+    @artist = Artist.new
   end
 
   def create
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to @artist
   end
 
   def edit
+    set_artist
   end
 
   def update
+    set_artist
+    set_artist.update(artist_params)
+    redirect_to @artist
   end
 
   private
