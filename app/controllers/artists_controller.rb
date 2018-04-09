a56395df3ec9c4c8c34db738fba2449048be893e
Chely Ho <adept_inept@yahoo.com>
2018-04-09 16:02:03 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,30 @@
 class ArtistsController < ApplicationController
+  # def index
+  # end
+
+  def show
+    set_artist
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
 end
