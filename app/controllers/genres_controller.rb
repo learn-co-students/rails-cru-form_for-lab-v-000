148204c52ebfc3f8f92449679b0cca865fdaabda
Chris Harvey <chharvey@icloud.com>
2018-04-17 23:16:03 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -5,28 +5,28 @@ class GenresController < ApplicationController
   end
 
   def create
-    @genre = Genre.create(genre_params(:name))
+    @genre = Genre.new(genre_params(:name))
+    @genre.save
     redirect_to genre_path(@genre)
   end
 
   def show
-    @genre = Genre.find_by(id: params[:id])
+    @genre = Genre.find(params[:id])
   end
 
   def edit
-    @genre = Genre.find_by(id: params[:id])
+    @genre = Genre.find(params[:id])
   end
 
   def update
-    @genre = Genre.find_by(id: params[:id])
+    @genre = Genre.find(params[:id])
     @genre.update(genre_params(:name))
     redirect_to genre_path(@genre)
   end
 
-private
 
-  def genre_params(*args)
-    params.require(:genre).permit(*args)
-  end
-
+  private
+    def genre_params(*args)
+      params.require(:genre).permit(*args)
+    end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,33 +4,32 @@ class SongsController < ApplicationController
     @songs = Song.all
   end
 
+  def show
+    @song = Song.find(params[:id])
+  end
+
   def new
     @song = Song.new
   end
 
   def create
-    @song = Song.create(song_params(:name, :artist_id, :genre_id))
+    @song = Song.new(song_params(:name, :artist_id, :genre_id))
+    @song.save
     redirect_to song_path(@song)
   end
 
-  def show
-    @song = Song.find_by(id: params[:id])
-  end
-
   def edit
-    @song = Song.find_by(id: params[:id])
+    @song = Song.find(params[:id])
   end
 
   def update
-    @song = Song.find_by(id: params[:id])
+    @song = Song.find(params[:id])
     @song.update(song_params(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
 
-private
-
-  def song_params(*args)
-    params.require(:song).permit(*args)
-  end
-
+  private
+    def song_params(*args)
+      params.require(:song).permit(*args)
+    end
 end
