4b33ca37a8898a03db4a41b58e67e2141e10a2f1
AndrewHongUW <32497926+AndrewHongUW@users.noreply.github.com>
2018-04-19 13:14:16 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419195441) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20180419185411) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
