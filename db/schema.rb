49a72cf24834a5c9c9f40ffa7781dccf967b37d6
AndrewHongUW <32497926+AndrewHongUW@users.noreply.github.com>
2018-04-19 13:07:50 -0700

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180419185411) do
+ActiveRecord::Schema.define(version: 20180419195441) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
---
+++ spec/views/articles/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "articles/new", type: :view do
+  before(:each) do
+    assign(:article, Article.new(
+      :title => "MyString",
+      :body => "MyText"
+    ))
+  end
+
+  it "renders new article form" do
+    render
+
+    assert_select "form[action=?][method=?]", articles_path, "post" do
+
+      assert_select "input#article_title[name=?]", "article[title]"
+
+      assert_select "textarea#article_body[name=?]", "article[body]"
+    end
+  end
+end
