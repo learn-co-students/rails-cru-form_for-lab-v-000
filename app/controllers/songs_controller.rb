f6025f8b56b0aa95f202f1cbae92bcb633ad397d
Elyse Klova <elyse.klova@gmail.com>
2018-04-19 20:23:49 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -29,6 +29,6 @@ class SongsController < ApplicationController
 
   private
   def song_params
-    params.require(:song).permit(:name, :artist, :genre)
+    params.require(:song).permit(:name, :artist_id, :genre_id)
   end
 end
