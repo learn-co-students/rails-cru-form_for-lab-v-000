d912fbe2c87a799a95b842d6631816a329c2924a
ElleeB <laura.trager@gmail.com>
2018-04-27 16:56:18 -0400

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,46 @@
+require 'pry'
+
+class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.create(song_params(:name, :genre_id, :artist_id))
+    @genre = Genre.find_by_id(@song.genre_id)
+    @artist = Artist.find_by_id(@song.artist_id)
+
+    redirect_to song_path(@song)
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params(:name, :genre_id, :artist_id))
+    @genre = Genre.find_by_id(@song.genre_id)
+    @artist = Artist.find_by_id(@song.artist_id)
+    
+    redirect_to song_path(@song)
+  end
+
+  def show
+    @song = Song.find(params[:id])
+    @genre = Genre.find_by_id(@song.genre_id)
+    @artist = Artist.find_by_id(@song.artist_id)
+  end
+
+  private
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
+
+end
