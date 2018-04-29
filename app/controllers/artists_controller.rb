c216c71eed2c7fa7137c941b649340fddbf6855d
AReid987 <read.musik@gmail.com>
2018-04-29 14:20:55 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -23,6 +23,10 @@ class ArtistsController < ApplicationController
   end
 
   def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params)
+
+    redirect_to artist_path(@artist)
   end
 
   private
