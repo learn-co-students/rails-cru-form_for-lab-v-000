d5ab22f5d86baed05f263de7bda5568342f9e39b
Eden <emesfun@outlook.com>
2018-04-30 17:05:18 -0400

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,26 @@
+class ArtistsController < ApplicationController 
+
+    def index
+        @allartists = Artist.all
+    end
+
+    def show 
+        @artist = Artist.find(params["id"])
+    end
+
+    def new 
+    end
+
+    def create 
+        @artist = Artist.new 
+        @artist.name = params["artist"]["name"]
+        @artist.bio = params["artist"]["bio"]
+        @artist.save
+
+        redirect_to artists_path(@artist)
+    end 
+    
+    def edit 
+        @artist = Artist.find(params["id"])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :artists, only: [:index, :show, :new, :create, :edit]
+
+  resources :genres, only: [:index, :show, :new, :create, :edit]
+
+  resources :songs, only: [:index, :show, :new, :create, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
