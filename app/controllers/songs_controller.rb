daa7a256892e0712aa573cd285dfde7542047063
Eden <emesfun@outlook.com>
2018-04-30 20:35:14 -0400

    working on lab

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,15 @@
 class SongsController < ApplicationController 
+
+    def index
+        @allsongs = Song.all
+    end
+
+    def show 
+        @song = Song.find(params[:id])
+
+        @genre = Genre.find(@song.genre_id)   
+
+        @artist = Artist.find(@song.artist_id)
+
+    end
 end
