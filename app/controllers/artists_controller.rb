74dabf84a3a9953321c6e653df8c10865caa3fab
ryanwevans <ryanwevans@icloud.com>
2018-05-08 17:15:51 -0700

    add #create, #update, #show and private/#post_params to controllers

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,24 @@
 class ArtistsController < ApplicationController
+  def create
+    @artist = Artist.new(post_params(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(post_params(:bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+
+  private
+
+  def post_params(*args)
+    params.require(:artist).permit(*args)
+  end
 end
