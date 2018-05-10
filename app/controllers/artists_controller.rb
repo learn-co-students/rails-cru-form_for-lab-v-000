dc642b2ad029cff889d310f9e6043facd67342cb
Aly Alidina <aly_alidina@hotmail.com>
2018-05-10 23:36:50 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,21 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def new
+    @artist = Artist.new
   end
 
   def create
+    @artist = Artist.find_by(params[:id])
   end
 
   def show
+    @artist = Artist.find_by(params[:id])
+  end
+
+  def update
+    @artist = Artist.find_by(params[:id])
   end
 end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,13 +1,21 @@
 class GenresController < ApplicationController
   def index
+    @genre = Genre.all
   end
 
   def new
+    @genre = Genre.new
   end
 
   def create
+    @genre = Genre.find_by(params[:id])
   end
 
   def show
+    @genre = Genre.find_by(params[:id])
+  end
+
+  def update
+    @genre= Genre.find_by(params[:id])
   end
 end
