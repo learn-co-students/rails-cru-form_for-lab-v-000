28d11f574dceead7a99ad7890745c7fccafe7495
Denise Pendleton <dpendl1@hotmail.com>
2018-05-20 19:09:16 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,12 @@
 class ArtistsController < ApplicationController
   has_many :songs
   has_many :genres, through: :songs
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+  end
 end
