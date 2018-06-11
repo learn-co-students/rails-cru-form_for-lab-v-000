bf930da81fe35adc5df12fc3c2b3671a859dfcce
Araba <araba.taylor20@gmail.com>
2018-06-10 21:46:50 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,19 +13,25 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(params.require(:song).permit(:name,:artist_id, :genre_id))
+    @song = Song.new(song_params)
     @song.save
     redirect_to song_path(@song)
   end
 
   def update
     @song = Song.find(params[:id])
-    @song = Song.update(params.require(:song).permit(:name,:artist_id, :genre_id))
+    @song.update(song_params)
     redirect_to song_path(@song)
   end
 
   def edit
     @song = Song.find(params[:id])
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:name, :artist_id, :genre_id)
   end
 
 end
