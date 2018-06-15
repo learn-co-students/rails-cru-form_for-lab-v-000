03f695ebef9e180a59433fcdfe3a2a117b534bbd
Kevin Huang <yellow.khaos@gmail.com>
2018-06-15 04:10:23 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,4 @@
+class Song < ActiveRecord::Base
+  belongs_to :song
+  belongs_to :genre
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :artists
+  resources :songs
+  resources :genres
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
