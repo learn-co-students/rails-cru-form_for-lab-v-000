5a474d357287fa576c7e20511d9f6215786786e5
Emily Zhao <tong.zhao.92@gmail.com>
2018-06-26 19:29:03 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,24 @@
 class SongsController < ApplicationController
+  def show
+    @song = Song.find_by(id: params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.create(song_param)
+    redirect_to song_path(@song)
+  end
+
+  def edit
+    @song = Song.find_by(id: params[:id])
+  end
+
+  private
+
+  def song_param
+    params.require(:song).permit(:name, :genre_id, :artist_id)
+  end
 end
