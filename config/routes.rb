f43160590cead9cacf5318e4d605a089f6e72cb8
Nicholas Powers <njpowers7915@gmail.com>
2018-06-28 13:44:26 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :songs
-  resources :genres
-  resources :artists
+  resources :songs, only: [:index, :new, :create, :show, :edit, :update]
+  resources :genres, only: [:new, :create, :show, :edit, :update]
+  resources :artists, only: [:new, :create, :show, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
