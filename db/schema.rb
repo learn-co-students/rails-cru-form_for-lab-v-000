9c8d49298df8de12edd33513c9378534438178d7
Victoria Meng <nights1ststar@protonmail.com>
2018-07-01 22:32:28 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180701223041) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
