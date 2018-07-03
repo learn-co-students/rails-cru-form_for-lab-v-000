a1a13e8577e2f41b50e35716f711c80a3c96eef6
Vanessa Toro <vanetoro84@gmail.com>
2018-07-03 14:56:31 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,7 @@
 class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
 
   def show
     @song = Song.find(params[:id])
@@ -9,8 +12,18 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(params.require(:song).permit(:name))
+    @song = Song.new(params.require(:song).permit(:name, :artist_id, :genre_id))
     @song.save
+    redirect_to song_path(@song)
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(params.require(:song).permit(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
 end
