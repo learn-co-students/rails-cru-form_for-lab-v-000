04b5e1cf6f8a9e7a112df7c044fbf8680f40a67c
Matt Farmer <mrfarmer777@gmail.com>
2018-07-11 04:55:40 +0000

    Done.

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @artist=Artist.create(name:"Adele", bio:"Sings a lot")
+    @genre=Genre.create(name:"Grindcore")
+
+  end
+
+  it "belongs to an artist instance" do
+    @song=Song.create(name:"Pickjuice",artist_id:1, genre_id:1)
+    expect(@song.artist.name).to eq("Adele")
+  end
 end
