f357db6d0ee053f256a92493e85e80a60d7dca1f
Neil Seejoor <nvseejoor@gmail.com>
2018-07-20 21:07:03 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,34 @@
 class GenresController < ApplicationController
+  def index
+    @genre = Grene.all
+  end
+
+  def show
+    @genre = Genre.find_by(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    @genre = Genre.find_by(params[:id])
+  end
+
+  def update
+    @genre = Genre.find_by(params[:id])
+    @genre.update(genre_params)
+    redirect_to genre_path(@genre)
+  end
+
+  private
+    def genre_params
+      params.require(:genre).permit(:name)
+    end
 end
