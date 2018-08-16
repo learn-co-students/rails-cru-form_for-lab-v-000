d0c055dc3e55af9e8c0846e6a0d291e688c952ab
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-16 19:12:31 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,14 +1,21 @@
 
 class GenresController < ApplicationController
-  before_action :find_genre , only: [:show, :update]
+  before_action :find_genre , only: [:show, :edit, :update]
   
   def show 
+  end
+  
+  def new
+    @genre = Genre.new
   end
   
   def create
     @genre = Genre.new(genre_params(:name))
     @genre.save
     redirect_to genre_path(@genre)
+  end
+  
+  def edit 
   end
   
   def update
