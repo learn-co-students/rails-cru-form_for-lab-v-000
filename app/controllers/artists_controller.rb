a7638c8689baa1d93f89441f296e5fcf68f96f17
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-09-05 22:31:25 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,25 @@
 class ArtistsController < ApplicationController
-  def index 
+  def index
+    @artists = Artist.all
   end 
   
   def show 
+    @artist = Artist.find(params[:id])
   end 
   
-  def new 
+  def new
+    @artist = Artist.new
   end 
   
   def create 
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params(:name, :bio))
+    redirect_to @artist
+  end
+  
+  private 
+  
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,4 @@
 class Song < ActiveRecord::Base
-  belongs_to :artist, :genre
+  belongs_to :artist
+  belongs_to :genre
 end
