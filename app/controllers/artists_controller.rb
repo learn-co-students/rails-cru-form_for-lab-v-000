278414ee2afd7352a5fe947b9e963a4fd827f31b
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-16 21:13:03 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,18 +1,15 @@
 class ArtistsController < ApplicationController
-
-
-  [:new, :create, :edit, :update, :show, :index]
-
   def index
     @artist = Artist.all
   end
 
   def new
-    @artist= Artist.create(artist_params(:name,:bio))
-    redirect_to artist_path(@artist)
+    @artist = Artist.new
   end
 
   def create
+    @artist= Artist.create(artist_params(:name,:bio))
+    redirect_to artist_path(@artist)
   end
 
   def show
@@ -20,9 +17,13 @@ class ArtistsController < ApplicationController
   end
 
   def edit
+    @artist = Artist.find(params[:id])
   end
 
   def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params(:name,:bio))
+    redirect_to artist_path(@artist)
   end
 
   def artist_params(*args)
