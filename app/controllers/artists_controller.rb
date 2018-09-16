86a30a807caa4136255db737ac6527c8281ae086
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-16 20:12:17 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,30 @@
 class ArtistsController < ApplicationController
-  
+
+
+  [:new, :create, :edit, :update, :show, :index]
+
+  def index
+    @artist = Artist.all
+  end
+
+  def new
+    @artist= Artist.create(artist_params(:name,:bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
 end
