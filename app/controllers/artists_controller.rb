9efabf38dfebfb4be16e1adf688d9ce03f251960
Savannah Younts <savyounts@gmail.com>
2018-09-20 19:35:17 +0000

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,38 @@
+class ArtistsController < ApplicationController
+
+  def index
+      @artists = Artist.all
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create(artist_params(:name, :bio))
+    redirect artist_path(@artist)
+  end
+
+  def edit
+    set_artist
+  end
+
+  def update
+    set_artist
+    @artist.update(artist_params(:name, :bio))
+    redirect artist_path(@artist)
+  end
+
+  def show
+    set_artist
+  end
+
+  private
+    def set_artist
+      @artist = Artist.find(params[:id])
+    end
+
+    def artist_params(*args)
+      params.require(:artist).permit(*args)
+    end
+end
---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,38 @@
+class GenresController < ApplicationController
+
+  def index
+      @genres = Genre.all
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.create(genre_params(:name))
+    redirect genre_path(@genre)
+  end
+
+  def edit
+    set_genre
+  end
+
+  def update
+    set_genre
+    @genre.update(genre_params(:name))
+    redirect genre_path(@genre)
+  end
+
+  def show
+    set_genre
+  end
+
+  private
+    def set_genre
+      @genre = Genre.find(params[:id])
+    end
+
+    def genre_params(*args)
+      params.require(:genre).permit(*args)
+    end
+end
