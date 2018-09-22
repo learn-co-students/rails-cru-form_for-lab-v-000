bc5f4900d9fd1dc801504559734c497ddf225fd4
Anu Khambete <anukhambete@gmail.com>
2018-09-22 07:09:48 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -12,9 +12,13 @@ class GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre)
   end
 
   def index
