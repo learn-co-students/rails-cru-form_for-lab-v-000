4d9163448ee9de3b7558cc3d5d4e8f59d66de033
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 05:51:45 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,29 @@
 class GenresController < ApplicationController
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new
+    @genre.name = params[:genre][:name]
+    if @genre.save
+      redirect_to genre_path(@genre)
+    end
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(name: params[:genre][:name])
+    redirect_to genre_path(@genre)
+  end
+
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,35 @@
 class SongsController < ApplicationController
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new
+    @song.name = params[:song][:name]
+    @song.artist_id = params[:song][:artist_id]
+    @song.genre_id = params[:song][:genre_id]
+    if @song.save
+      redirect_to song_path(@song)
+    end
+  end
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(name: params[:song][:name], artist_id: params[:song][:artist_id], genre_id: params[:song][:genre_id])
+    redirect_to song_path(@song)
+  end
+
 end
