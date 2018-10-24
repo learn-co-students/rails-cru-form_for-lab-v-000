e3102f59fe2f526f482341124a94d200d29e4a87
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-24 18:53:29 +0000

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class ArtistsController < ApplicationController
+  
+  def new
+    @artist = Artist.new
+  end
+  
+  def show
+    @artist = Artist.find(params[:id])
+  end
+  
+  def create
+    @artist = Artist.new(params.require(:artist).permit(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def edit 
+    @artist = Artist.find(params[:id])
+  end
+  
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+  
+end
