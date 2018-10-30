a98390ccb7c4e2d2dadffb8d85e40a924c9fc117
rina71 <yurinas@gmail.com>
2018-10-30 03:38:13 -0400

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,33 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(post_params(:name, :artist_id, :genre_id))
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+  def show
+    @song = Song.find_by_id(params[:id])
+  end
+
+  def edit
+    @song = Song.find_by_id(params[:id])
+  end
+
+  def update
+    @song = Song.find_by_id(params[:id])
+    @song.update(post_params(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
+  end
+
+    private
+    def post_params(*args)
+      params.require(:song).permit(*args)
+    end
+end
