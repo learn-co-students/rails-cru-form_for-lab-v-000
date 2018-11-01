bf314c81d33daeef0be67e2df24005d3c3e0e71d
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-01 21:25:55 +0000

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,6 @@
 class GenresController < ApplicationController
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181101205939) do
+ActiveRecord::Schema.define(version: 20181101211218) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -28,8 +28,8 @@ ActiveRecord::Schema.define(version: 20181101205939) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "name"
-    t.integer  "artist_id"
     t.integer  "genre_id"
+    t.integer  "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
