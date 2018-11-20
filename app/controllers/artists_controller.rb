e039f917cca50c531ac34263ed90403fd0a4c444
amitnyc83 <amit_nyc@live.com>
2018-11-20 12:46:06 -0500

    create, read and update artist & genre

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,39 @@
 class ArtistsController < ApplicationController
+
+
+
+  def new
+    @artist = Artist.new
+  end
+
+
+  def create
+    @artist = Artist.new(artist_params(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+
+  private
+
+  def artist_params(*arg)
+    params.require(:artist).permit(*arg)
+  end
+
 end
