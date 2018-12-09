3a032da30d47059fffe4fbd9b4cb55efda65f3c3
mrjak13 <justinkenney13@gmail.com>
2018-12-09 09:15:01 -0800

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,18 @@
+class GenresController < ApplicationController
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+    
+end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,18 @@
+class SongsController < ApplicationController
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+    
+end
