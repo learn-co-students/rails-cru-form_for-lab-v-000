ac97b12ba57dc5ab5ec086d3b7f8c16036440a9b
Justine B <justinegayton@gmail.com>
2018-12-21 20:29:55 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name: "Dr.Dre", bio: "Uber cool")
+Genre.create(name: "rap")
+Song.create(name: "singsong", artist_id: 1, genre_id: 1)
+
