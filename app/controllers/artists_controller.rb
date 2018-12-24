1fa211a0e62890bca943fb0e50a863912dbc94f0
RonSala <ron.sala@gmail.com>
2018-12-24 04:01:56 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,19 +1,29 @@
 class ArtistsController < ApplicationController
   def new
+    @artist = Student.new
   end
 
   def create
+    @artist = Student.new(params.require(:artist))
+    @artist.save
+    redirect_to artist_path(@artist)
   end
 
   def edit
+    @artist = Student.find(params[:id])
   end
 
   def update
+    @artist = Student.find(params[:id])
+    @artist.update(params.require(:artist))
+    redirect_to artist_path(@artist)
   end
 
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Student.find(params[:id])
   end
 end
