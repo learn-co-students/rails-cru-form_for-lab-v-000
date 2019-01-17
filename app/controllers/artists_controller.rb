b49149bae65825fe0bbc12cd5b5e5f994b7baae0
Barbara Choy <barbchoy@gmail.com>
2019-01-17 18:19:58 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,42 @@
 class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def create
+    @artist = Artist.new(artist_params())
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def edit
+
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params())
+    redirect_to artist_path(@artist)
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id])
+    @artist.destroy
+    redirect_to artists_path
+  end
+
+  private
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
+  end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,30 @@
 class SongsController < ApplicationController
+
+  def index
+
+  end
+
+  def create
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
