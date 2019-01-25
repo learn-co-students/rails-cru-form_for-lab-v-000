fce3d32914bceee9e8e277e5fc7da3bf7599b6ac
Naudria <rima.jean@gmail.com>
2019-01-25 13:56:03 -0600

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190124225429) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string   "name"
+    t.text     "bio"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string  "name"
+    t.integer "artist_id"
+    t.integer "genre_id"
+  end
 
 end
