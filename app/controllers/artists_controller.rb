a39c48cb14f210fff98eb90e3df64c21b6e311e3
Jonathan Chan <johnkchan252@gmail.com>
2019-02-02 03:45:43 +0000

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,37 @@
+class ArtistsController < ApplicationController
+  
+  def new
+    @artist = Artist.new
+  end
+  
+  def create
+    @artist = Artist.new(post_params(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+  
+  def index
+    @artists = Artist.all
+  end
+  
+  def show
+    @artist = Artist.find(params[:id])
+  end
+  
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+  
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(post_params(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+  
+  private
+  
+  def post_params(*args)
+    params.require(:artist).permit(*args)
+  end
+
+end
