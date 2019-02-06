17d8c428c930d4636bb2f7022524024f9930f2b0
Ayana Bando <ayanaflax@yahoo.com>
2019-02-06 16:34:12 +0000

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,36 @@
+class ArtistsController < ApplicationController
+
+ def new
+   @artists = Artist.new
+ end
+ 
+  def show
+    get_artist
+  end
+  
+  def edit
+    get_artist
+  end
+  
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+  
+  def update
+    get_artist
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
+  end
+   
+private
+   
+ def get_artist
+   @artist = Artist.find(params[:id])
+ end
+
+  def artist_params
+    params.require(:artist).permit(:name, :bio)
+  end
+end
