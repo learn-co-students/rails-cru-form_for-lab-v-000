b2e5aae3562da355acd2ca022887ed34eda11af8
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-02-06 23:45:24 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get "artists/:id", to: "artists#show"
+  resources :artists, except: [:delete, :index]
+  resources :genres, except: [:delete, :index]
+  resources :songs, except: [:delete]
+
+  #get "artists/:id", to: "artists#show", as: 'artist'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
