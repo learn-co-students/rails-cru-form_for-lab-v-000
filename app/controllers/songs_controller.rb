b5344d314966b7ec855546da8f475ea4783549bc
github username <github email address>
2019-02-13 13:54:58 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,7 @@ class SongsController < ApplicationController
 	end
 
 	def create
-		@song = Song.new(song_params(:name, :bio))
+		@song = Song.new(song_params(:name, :artist_id, :genre_id))
 		@song.save
 		redirect_to song_path(@song)
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :songs, only: [:show, :new, :create, :edit, :update]
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
   resources :genres, only: [:show, :new, :create, :edit, :update]
   resources :artists, only: [:show, :new, :create, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
