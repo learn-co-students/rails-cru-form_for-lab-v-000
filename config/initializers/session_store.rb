0bf86c39f7087c28caae1786ae62836ea7b2db92
maxwellbenton <maxwellbenton@gmail.com>
2019-02-13 15:26:29 -0500

    rails app:update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_beats-by-rails_session'
+Rails.application.config.session_store :cookie_store, key: '_beats_by_rails_session'
