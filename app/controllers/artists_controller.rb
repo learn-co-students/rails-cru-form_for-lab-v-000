667b031e6073c798a11582d5e8b5fec6ac7d023d
thesilentc <thesilentc@me.com>
2019-02-13 13:30:58 -0500

    delete reference

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,53 @@
+class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def show
+    # binding.pry
+    @artist = Artist.find_by(params[:id])
+    # @artist = Artist.find_by(artist_params)
+
+  end
+
+  def edit
+    get_artist
+  end
+
+  def update
+    get_artist
+    @artist.update(artist_params)
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def delete
+    get_artist
+    @artist.delete
+    redirect_to artist_path
+  end
+
+  private
+
+    def artist_params(*args)
+      # binding.pry
+      params.require(:artist).permit(:name, :bio)
+    end
+
+    def get_artist
+      @artist = Artist.find_by(params[:id])
+    end
+
+
+end
