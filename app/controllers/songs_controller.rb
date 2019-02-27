c1a20865121d247c18d6013bcad99855286c9a78
Srqrme <srqrme@gmail.com>
2019-02-27 12:35:31 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,6 +18,10 @@ class SongsController < ApplicationController
     redirect_to song_path(@song)
   end
 
+  def edit
+    @song = Song.find(params[:id])
+  end
+
   def update
     @song = Song.find(params[:id])
     @song.update(song_params)
