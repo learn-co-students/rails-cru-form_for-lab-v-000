5d2132e1bd89a0b74515932f0b9886c66e0875f9
Heather Miller <heather.miller114@gmail.com>
2019-03-14 19:28:01 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,6 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190314230600) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.text   "bio"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string  "name"
+    t.integer "artist_id"
+    t.integer "genre_id"
+  end
 
 end
