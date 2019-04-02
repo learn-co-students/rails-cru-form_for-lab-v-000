cef084a13e86702b5d966efa286a5d4f264f44f2
Ashley Wong <ashwong04@gmail.com>
2019-04-02 23:55:40 +0000

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,12 @@
+class ArtistsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190402234837) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "bio"
+  end
 
 end
