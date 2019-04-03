c0ee61969eb4c900083c25ccce4dbb01ecd73851
Ashley Wong <ashwong04@gmail.com>
2019-04-03 02:05:12 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,9 +4,27 @@ class ArtistsController < ApplicationController
   end
 
   def new
+    @artist = Artist.new
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
-  
+
+  def create
+    @artist = Artist.new(name: params[:artist][:name], bio: params[:artist][:bio])
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
 end
