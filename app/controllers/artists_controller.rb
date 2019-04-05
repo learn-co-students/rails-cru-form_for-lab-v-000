b5f28088205140d721bc1a60f5c386eeea73e7a4
Samantha brake <sammy.brake@gmail.com>
2019-04-05 22:50:30 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,4 +4,23 @@ class ArtistsController < ApplicationController
   def show
     @artist = Artist.find_by(params[:id])
   end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+    @artist = Artist.find_by(params[:id])
+  end
+
+  def update
+    @artist = Artist.find_by(params[:id])
+    @artist = Artist.update(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
 end
--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,4 +4,23 @@ class GenresController < ApplicationController
     @genre = Genre.find_by(params[:id])
   end
 
+  def new
+    @genre = Genre.new
+  end
+
+  def edit
+      @genre = Genre.find_by(params[:id])
+  end
+
+  def create
+    @genre = Genre.create(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  def update
+    @genre = Genre.find_by(params[:id])
+    @genre = Genre.update(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre)
+  end
+
 end
