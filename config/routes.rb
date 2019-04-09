059fe9ed7a57317c76774627a11df66f4276ce49
Christine Cua <cloudycua@gmail.com>
2019-04-09 17:53:14 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,5 @@
 Rails.application.routes.draw do
-  get 'genres/index'
-
-  get 'genres/show'
-
-  get 'genres/new'
-
-  get 'genres/edit'
-
-  get 'genres/create'
-
-  get 'genres/update'
-
-  get 'songs/index'
-
-  get 'songs/show'
-
-  get 'songs/new'
-
-  get 'songs/edit'
-
-  get 'songs/create'
-
-  get 'songs/update'
-
-  get 'artists/index'
-
-  get 'artists/show'
-
-  get 'artists/new'
-
-  get 'artists/edit'
-
-  get 'artists/create'
-
-  get 'artists/update'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists, only: %i[index show new edit create update]
+  resources :songs, only: %i[index show new edit create update]
+  resources :genres, only: %i[index show new edit create update]
 end
