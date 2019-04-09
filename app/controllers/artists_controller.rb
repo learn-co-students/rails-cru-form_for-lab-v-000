1ce82c90435b88e9768c7eb512c4f48d3278fc38
github username <github email address>
2019-04-08 20:07:23 -0400

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,30 @@
+class ArtistsController < ApplicationController
+
+def index
+end
+
+def new
+  @artist = Artist.new
+end
+
+def show
+  @artist = Artist.find(params[:id])
+end
+
+def create
+  @artist = Artist.new(name: params[:artist][:name], bio: params[:artist][:bio])
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+      @artist = Artist.find(params[:id])
+    end
+
+    def update
+      @artist = Artist.find(params[:id])
+      @artist.update(params.require(:artist).permit(:name, :bio))
+      redirect_to artist_path(@artist)
+    end
+
+  end
---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,30 @@
+class GenresController < ApplicationController
+  def index
+   end
+
+   def new
+     @genre = Genre.new
+   end
+
+   def create
+     @genre = Genre.new(name: params[:genre][:name])
+     @genre.save
+     redirect_to genre_path(@genre)
+   end
+
+   def show
+     @genre = Genre.find(params[:id])
+   end
+
+   def edit
+     @genre = Genre.find(params[:id])
+   end
+
+   def update
+     @genre = Genre.find(params[:id])
+     @genre.update(params.require(:genre).permit(:name))
+     redirect_to genre_path(@genre)
+   end
+
+
+end
