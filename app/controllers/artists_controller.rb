e8a5f72df026affd51abad7bbedbe46e93bb131b
Christine Cua <cloudycua@gmail.com>
2019-04-09 17:52:06 +0000

    artists controller and views

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,19 +1,35 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
+    @artist = Artist.new
   end
 
   def edit
+    @artist = Artist.find(params[:id])
   end
 
   def create
+    @artist = Artist.create(artist_params)
+    redirect_to artist_path(@artist)
   end
 
   def update
+    @artist = Artist.find(params[:id])
+    @artist = Artist.upate(artist_params)
+    redirect_to artist_path(@artist)
   end
+
+  private
+
+    def artist_params
+      params.require(:artist).permit!
+    end
+
 end
