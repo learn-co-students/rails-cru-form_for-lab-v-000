2e370a47aa54523f2c828d70ee9c8d072d932b28
Meg Gutshall <meghan.gutshall@gmail.com>
2019-04-14 20:39:26 -0400

    Draw routes and run migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :songs
-  resources :genres
-  resources :artists
+  resources :songs, except: :destroy
+  resources :genres, except: :destroy
+  resources :artists, except: :destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
