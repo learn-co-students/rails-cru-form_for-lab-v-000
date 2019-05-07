7eff9271f85c6194ac6a621629d865287f212bd9
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-05-06 20:18:41 -0700

    Done.

---
+++ db/migrate/20190506030726_create_artists.rb
@@ -0,0 +1,11 @@
+class CreateArtists < ActiveRecord::Migration[5.0]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.text :bio
+
+      t.timestamps
+    end
+  end
+end
