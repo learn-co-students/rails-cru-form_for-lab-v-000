9cde9ad849227046708875d524d00cd804ccb7d7
ahotchkin <ahotchk1@yahoo.com>
2019-05-12 11:30:16 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+hanson = Artist.create(name: "Hanson", bio: "3 brothers from Tulsa, OK")
+pop = Genre.create(name: "Pop")
+mmmbop = Song.create(name: "MMMBop")
