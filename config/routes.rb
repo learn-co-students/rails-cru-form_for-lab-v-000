c858993909492aa2229536932d6c5d60a45ea8f9
Lois Lee <lois.lee@me.com>
2019-05-16 20:15:28 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :artists, except: [:delete, :index]
+  resources :genres, except: [:delete, :index]
+  resources :songs, except: [:delete]
 end
