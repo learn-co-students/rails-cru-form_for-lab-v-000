fcb872947e88e4cb3672e96c6657a19b43d4260f
Tom Cho <tc.thomascho@gmail.com>
2019-06-02 17:35:43 -0400

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,36 @@
+class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.create(song_params)
+    redirect_to song_path(@song)
+  end
+
+  def show
+    @song = Song.find_by(id: params[:id])
+  end
+
+  def edit
+    @song = Song.find_by(id: params[:id])
+  end
+
+  def update
+    @song = Song.find_by(id: params[:id])
+    @song.update(song_params)
+    redirect_to song_path(@song)
+  end
+
+  private
+
+    def song_params
+      params.require(:song).permit!
+    end
+
+end
