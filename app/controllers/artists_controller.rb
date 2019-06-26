f586841d9815f5e595a36f977f29b72c0fc92a16
github username <github email address>
2019-06-26 17:50:30 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,4 +3,9 @@ class ArtistsController < ApplicationController
     def show
         @artist = Artist.find(params[:id]) 
     end
+
+    def new
+        @artist = Artist.find(params[:id])
+    end
+    
 end
