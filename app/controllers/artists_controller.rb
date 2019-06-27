ba65a2ea0911ca369c8d00d631ceabcabd8e619f
christianavdm <christianavdm@gmail.com>
2019-06-27 15:25:20 -0700

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,29 @@
 class ArtistsController < ApplicationController
+    def index
+		@artists = Artist.all
+	end
+
+	def show
+		@artist = Artist.find(params[:id])
+	end
+
+	def new
+		@artist = Artist.new
+	end
+
+	def create
+	  @artist = Artist.new(params[:artist])
+	  @artist.save
+	  redirect_to artist_path(@artist)
+	end
+
+	def edit
+	    @artist = artist.find(params[:id])
+	end
+
+	def update
+		@artist = artist.find(params[:id])
+		@artist.update(params.require(:artist).permit(:name, :bio))
+	  redirect_to artist_path(@artist)
+	end
 end
