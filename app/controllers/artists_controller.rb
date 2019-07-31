b65a1bd6fe1a3e2a7615d6dd345c75650075bdb2
Paul Fell <swellpf@gmail.com>
2019-07-31 15:11:22 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,29 @@
 class ArtistsController < ApplicationController
+
+    def index
+        @artists = Artist.all
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def show
+        @artist = Artist.find(params[:id])
+    end
+
+    def create
+        @artist = Artist.new(artist_params)
+        @artist.save
+        redirect_to artist_path(@artist)
+    end
+
+
+
+
+
+    private
+        def artist_params
+            params.require(:artist).permit(:name, :bio)
+        end
 end
