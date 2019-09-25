c897848c0cb98d41afed816b521f398d999ffdcb
Amil <github email address>
2019-09-25 19:42:33 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # resources :genres
+  # resources :artists
+  # resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs, except: [:update, :destroy]
+  resources :genres, except: [:update, :destroy]
+  resources :artists, except: [:update, :destroy]
 end
