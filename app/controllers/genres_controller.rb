67085278b3ebde359835cc160d36470591b0be22
sdcro <sdcrouse91@gmail.com>
2019-11-27 17:52:20 -0800

    Done.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -10,4 +10,27 @@ class GenresController < ApplicationController
   def new
     @genre = Genre.new
   end
+
+  def create
+    genre = Genre.create(genre_params)
+
+    redirect_to genre_path(genre)
+  end
+
+  def edit
+    @genre = Genre.find_by_id(params[:id])
+  end
+
+  def update
+    genre = Genre.find_by_id(params[:id])
+    genre.update(genre_params)
+    
+    redirect_to genre_path(genre)
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191127082306) do
+ActiveRecord::Schema.define(version: 20191127211518) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20191127082306) do
 
   create_table "genres", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "artist_id"
+    t.integer  "genre_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
