743c24692691254442a0a85bfcdf1f90588cac29
Lynie McClimon <vmcclimon33@yahoo.com>
2019-12-11 02:26:05 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,35 @@
 class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    @song.save
+    redirect_to_song_path(@song)
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.new(song_params)
+    @song.update(song_params)
+    redirect_to_song_path(@song)
+  end
+
+  private
+
+    def song_params
+      params.require(:song).permit(:name)
+    end
 end
