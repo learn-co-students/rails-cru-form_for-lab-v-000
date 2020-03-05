681bc588c8eef78903c059727060618fd732283c
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-05 18:54:43 +0000

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,20 @@
+class ArtistsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def updated
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :artists, only: [:index, :show, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
