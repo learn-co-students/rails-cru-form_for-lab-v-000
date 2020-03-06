809718cada6425070b6e9fbeeee3ac8d7f4c7622
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-06 02:48:08 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :artists, only: [:index, :show, :new, :create, :edit, :update]
+  resources :artists, only: [:show, :new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
 end
