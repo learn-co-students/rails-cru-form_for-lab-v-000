695827bc2848d51fc5e643bc53240255cf61fa74
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-06 02:41:41 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,18 +3,33 @@ class ArtistsController < ApplicationController
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
+    @artist = Artist.new
   end
 
   def create
+    @artist = Artist.new(artist_params(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
   end
 
   def edit
+    @artist = Artist.find(params[:id])
   end
 
-  def updated
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(artist_params(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  private
+
+  def artist_params(*args)
+    params.require(:artist).permit(*args)
   end
 
 end
