82f743fb1067b20a321516ab01c27e2a9ff7072f
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-27 23:02:11 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,6 +14,8 @@ class SongsController < ApplicationController
     def create
         @song = Song.new
         @song[:name] = params[:song][:name]
+        @song[:artist_id] = params[:song][:artist_id]
+        @song[:genre_id] = params[:song][:genre_id]
         @song.save
         redirect_to song_path(@song)
     end
