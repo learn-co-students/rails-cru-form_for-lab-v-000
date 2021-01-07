4b37f180471ed144cbfd301665dcae5afde9dae9
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2021-01-07 18:10:32 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :accounts
+  get 'admin/dashboard'
+
+  get 'admin/stats'
+
+  get 'admin/financials'
+
+  get 'admin/settings'
+
+  get 'categories/show'
+
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :categories
 end
