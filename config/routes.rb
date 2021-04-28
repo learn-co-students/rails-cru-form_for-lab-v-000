60cd5f0c84156c0cc1b9b4297fb4dfd7477779fd
Jesse Wenger <wengerj@gmail.com>
2021-04-28 03:04:20 +0000

    db, models, index, show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :songs
+  resources :genres
+  resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+#rails g resource Artist name:string bio:text --no-test-framework
+#rails g resource Genre name:string --no-test-framework
+#rails g resource Song name:string artist_id:integer genre_id:integer --no-test-framework
