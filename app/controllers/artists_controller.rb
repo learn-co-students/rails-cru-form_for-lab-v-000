dfa5cf5780156293b12f09f247a3a77d4c2396aa
Jesse Wenger <wengerj@gmail.com>
2021-04-29 01:41:13 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,7 +3,33 @@ class ArtistsController < ApplicationController
     @artists = Artist.all
   end
 
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(params_artist(:name, :bio))
+    @artist.save
+    redirect_to artist_path(@artist)
+  end
+
   def show
     @artist = Artist.find(params[:id])
+  end
+
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(params_artist(:name, :bio))
+    redirect_to artist_path(@artist)
+  end
+
+  private
+
+  def params_artist(*args)
+    params.require(:artist).permit(*args)
   end
 end
