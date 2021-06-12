6fb26d2b289aed2ca2b2c0a2b582a9aaa8392b17
Shasha Yan <shashayan@gmail.com>
2021-06-12 00:19:41 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
+  resources :songs
+  resources :genres
+  resources :artists 
+  # get 'songs/edit'
+
+  # get 'songs/index'
+
+  # get 'songs/new'
+
+  # get 'songs/show'
+
+  # get 'genres/edit'
+
+  # get 'genres/new'
+
+  # get 'genres/show'
+
+  # get 'artists/edit'
+
+  # get 'artists/new'
+
+  # get 'artists/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
